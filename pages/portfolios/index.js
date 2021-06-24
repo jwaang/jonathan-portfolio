ca546517124a1e1e47922d8ef917daaca7843d23
Jonathan Wang <jonathan.wang1996@gmail.com>
2021-06-23 23:04:38 -0500

    graphql beginnings

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,60 +1,152 @@
-const apiCall = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res({ testingData: "Just some testing Data" });
-    }, 200);
-  });
+import axios from "axios";
+import PortfolioCard from "@/components/portfolios/PortfolioCards";
+import Link from "next/link";
+import { useState } from "react";
+
+const fetchPortfolios = () => {
+  const query = `
+      query Portfolios {
+        portfolios {
+          _id
+          title
+          company
+          companyWebsite
+          location
+          jobTitle
+          description
+          startDate
+          endDate
+        }
+      }
+    `;
+  return axios
+    .post("http://localhost:3000/graphql", { query })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.portfolios);
 };
 
-const Portfolios = (props) => {
+const graphCreatePortfolio = () => {
+  const query = `
+      mutation CreatePortfolio {
+        createPortfolio(
+          input: {
+            title: "Job in Dallas"
+            company: "Citi"
+            companyWebsite: "www.citi.com"
+            location: "Dallas, TX"
+            jobTitle: "SWE"
+            description: "Yeet haw"
+            startDate: "01/01/2019"
+            endDate: "01/01/2021"
+          }
+        ) {
+          _id
+          title
+          company
+          companyWebsite
+          location
+          jobTitle
+          description
+          startDate
+          endDate
+        }
+      }
+    `;
+  return axios
+    .post("http://localhost:3000/graphql", { query })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.createPortfolio);
+};
+
+const graphUpdatePortfolio = (id) => {
+  const query = `
+    mutation UpdatePortfolio($id: ID) {
+      updatePortfolio(id: $id, input: { title: "Updated Title!" }) {
+          _id
+          title
+          company
+          companyWebsite
+          location
+          jobTitle
+          description
+          startDate
+          endDate
+      }
+    }
+    `;
+  const variables = { id };
+  return axios
+    .post("http://localhost:3000/graphql", { query, variables })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.updatePortfolio);
+};
+
+const graphDeletePortfolio = (id) => {
+  const query = `
+    mutation DeletePortfolio($id: ID) {
+      deletePortfolio(id: $id)
+    }
+    `;
+  const variables = { id };
+  return axios
+    .post("http://localhost:3000/graphql", { query, variables })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.deletePortfolio);
+};
+
+const Portfolios = ({ data }) => {
+  const [portfolios, setPortfolios] = useState(data.portfolios);
+
+  const createPortfolio = async () => {
+    const newPortfolio = await graphCreatePortfolio();
+    const newPortfolios = [...portfolios, newPortfolio];
+    setPortfolios(newPortfolios);
+  };
+
+  const updatePortfolio = async (id) => {
+    const updatedPortfolio = await graphUpdatePortfolio(id);
+    const index = portfolios.findIndex((p) => p._id === id);
+    const newPortfolios = [...portfolios];
+    newPortfolios[index] = updatedPortfolio;
+    setPortfolios(newPortfolios);
+  };
+
+  const deletePortfolio = async (id) => {
+    const deletedId = await graphDeletePortfolio(id);
+    console.log(deletedId);
+    let p = portfolios.filter((p) => p._id !== deletedId);
+    setPortfolios(p);
+  };
+
   return (
     <>
-      {props.testingData}
       <section className="section-title">
         <div className="px-2">
           <div className="pt-5 pb-4">
             <h1>Portfolios</h1>
           </div>
         </div>
+        <button onClick={createPortfolio} className="btn btn-primary">
+          Create Portfolio
+        </button>
       </section>
       <section className="pb-5">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
+          {portfolios.map((portfolio) => (
+            <div key={portfolio._id} className="col-md-4">
+              <Link href="/portfolios/[id]" as={`/portfolios/${portfolio._id}`}>
+                <a className="card-link">
+                  <PortfolioCard portfolio={portfolio} />
+                </a>
+              </Link>
+              <button onClick={() => updatePortfolio(portfolio._id)} className="btn btn-primary">
+                Update Portfolio
+              </button>
+              <button onClick={() => deletePortfolio(portfolio._id)} className="btn btn-primary">
+                Delete Portfolio
+              </button>
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
@@ -62,9 +154,8 @@ const Portfolios = (props) => {
 };
 
 Portfolios.getInitialProps = async () => {
-  console.log("GET INITIAL PROPS PORTFOLIOS");
-  const data = await apiCall();
-  return { ...data };
+  const portfolios = await fetchPortfolios();
+  return { data: { portfolios } };
 };
 
 export default Portfolios;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,59 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// resolvers
+const { portfolioQueries, portfolioMutations } = require("./graphql/resolvers");
+
+// types
+const { portfolioTypes } = require("./graphql/types");
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  // Construct a schema using GraphQL schema language
+  const typeDefs = gql`
+    ${portfolioTypes}
 
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    type Query {
+      hello: String
+      portfolio(id: ID): Portfolio
+      portfolios: [Portfolio]
+    }
+
+    type Mutation {
+      createPortfolio(input: PortfolioInput): Portfolio
+      updatePortfolio(id: ID, input: PortfolioInput): Portfolio
+      deletePortfolio(id: ID): ID
+    }
+  `;
+
+  // The root provides a resolver for each API endpoint
+  const resolvers = {
+    Query: {
+      ...portfolioQueries,
+    },
+    Mutation: {
+      ...portfolioMutations,
+    },
+  };
+
+  const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+  apolloServer.applyMiddleware({ app: server });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
